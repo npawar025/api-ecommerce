54540fc6ddcaa42272c2d5c7f4d1de46bd8b5915
Nilesh <npawar025@gmail.com>
2023-06-12 17:34:26 +0530

    fixed api doc

--- app.js
+++ app.js
@@ -35,20 +35,20 @@ app.use(xss());
 app.use(mongoSanitize());
 
 //middleware
-app.use(morgan("tiny"));
+// app.use(morgan("tiny"));
 app.use(express.json()); //to have access to json data in our app
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.static("./public"));
 app.use(fileUpload());
 
-app.get("/", (req, res) => {
-  res.send("ecom api");
-});
-app.get("/api/v1", (req, res) => {
-  // console.log(req.cookies);
-  console.log(req.signedCookies);
-  res.send("ecom api");
-});
+// app.get("/", (req, res) => {
+//   res.send("ecom api");
+// });
+// app.get("/api/v1", (req, res) => {
+//   // console.log(req.cookies);
+//   console.log(req.signedCookies);
+//   res.send("ecom api");
+// });
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
